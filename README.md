887fde4b353e6902c1f188a23b3279fb3007c74f
jirisitera <jirik.sitera@gmail.com>
2025-02-12 09:54:45 +0100

    major upgrades

---
+++ README.md
@@ -0,0 +1,7 @@
+# EV3 IoTcraft
+
+A simple example of how to connect the LEGO MINDSTORMS EV3 brick to an MQTT server.
+
+Requires adding the EV3DEV image to the brick using a microSD card as described in the [Getting Started with ev3dev](https://www.ev3dev.org/docs/getting-started/) guide.
+
+We recommend you use the [LEGO® MINDSTORMS® EV3 MicroPython](vscode:extension/lego-education.ev3-micropython) extension for Visual Studio Code.
--- main.py
+++ main.py
@@ -1,41 +1,42 @@
 #!/usr/bin/env pybricks-micropython
-from pybricks import ev3brick as brick
-from pybricks.ev3devices import Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor
-from pybricks.parameters import Port, Stop, Direction, Button, Color, SoundFile, ImageFile, Align
-from pybricks.tools import print, wait, StopWatch
-from pybricks.robotics import DriveBase
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
 from umqtt.robust import MQTTClient
 import time
-import os
+import random
+import string
 import config
 
 # https://testclient-cloud.mqtt.cool/
 
+ev3 = EV3Brick()
+
 motor = Motor(Port.A)
 
-MQTT_ClientID = 'ev3test'
+CLIENT_ID = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
 
-MQTT_Topic_Status = 'ev3/status'
-MQTT_Topic_Motor = 'ev3/motor'
+TOPIC_STATUS = 'ev3/status'
+TOPIC_MOTOR = 'ev3/motor'
 
-def getmessages(topic, msg):
-    if topic == MQTT_Topic_Status.encode():
-        brick.display.text(str(msg.decode()))
-    elif topic == MQTT_Topic_Motor.encode():
-        brick.display.text(str(msg.decode()))
+def callback(topic, msg):
+    if topic == TOPIC_STATUS.encode():
+        ev3.screen.print(str(msg.decode()))
+    elif topic == TOPIC_MOTOR.encode():
+        ev3.screen.print(str(msg.decode()))
         motor.run_target(180, int(msg.decode()))
 
 motor.reset_angle(0)
 
-client = MQTTClient(MQTT_ClientID, config.MQTT_BROKER)
+client = MQTTClient(CLIENT_ID, config.MQTT_BROKER)
 client.connect()
 
-client.publish(MQTT_Topic_Status, MQTT_ClientID + ' Started')
-client.set_callback(getmessages)
-client.subscribe(MQTT_Topic_Status)
-client.subscribe(MQTT_Topic_Motor)
-client.publish(MQTT_Topic_Status, MQTT_ClientID + ' Listening')
+client.publish(TOPIC_STATUS, CLIENT_ID + ' started...')
+client.set_callback(callback)
+client.subscribe(TOPIC_STATUS)
+client.subscribe(TOPIC_MOTOR)
+client.publish(TOPIC_STATUS, CLIENT_ID + ' listening...')
 
 while True:
     client.check_msg()
-    time.sleep(0.1)
+    time.sleep(0.2)
